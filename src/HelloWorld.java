0ede2ed108819da428c13cea4f227dfcc9ae3481
Andre Jones <andreij6@gmail.com>
2013-11-03 01:21:29 -0500

    Using classes and Objects

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,21 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		
+		//Using Classes and Objects
+		Welcomer welcomer = new Welcomer();
+		welcomer.sayHello();
+		
+		String welcome = "Hello!";
+		//Long HandVersion
+		//String welcome = new String("Hello!");
+		
+		char[] chars = {'H','e','l','l','o','!'};
+		String s = new String(chars);
+		System.out.println(s);  //Hello!
+		
 
 	}
+	
 
 }
---
+++ src/Welcomer.java
@@ -0,0 +1,8 @@
+
+public class Welcomer {
+	private String welcome = "Hello";
+	
+	public void sayHello() {
+		System.out.println(welcome);
+	}
+}
